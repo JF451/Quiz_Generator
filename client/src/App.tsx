5aabba4a238f9fe785d5d61d9b028aa912768595
Justin Fulkerson <jfulkerson451@gmail.com>
2026-08-21 03:01:04 -0700

    fixed routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,13 +7,23 @@ import TakeQuiz from "./pages/TakeQuiz";
 import AttemptHistory from "./pages/AttemptHistory";
 import AIGenerateQuiz from "./pages/AIGenerateQuiz";
 import Register from "./pages/Register";
+import { Navigate } from "react-router-dom";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route
+        path="/"
+        element={<Navigate to="/login" replace />}
+      />
+
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+      
         <Route
   path="/register"
   element={<Register />}
