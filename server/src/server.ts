829da84841883010ec9e28ab1276a1e079866557
Justin Fulkerson <jfulkerson451@gmail.com>
2026-08-21 02:56:25 -0700

    Configure production CORS

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,14 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://sweet-lokum-4ee80c.netlify.app",
+    ],
+  })
+);
 app.use(express.json());
 
 app.get("/api/health", (_req, res) => {
